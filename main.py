58eea1baac059aa3d8764d1aff8441710f45d797
yuriy.karakosov <yuriy.karakosov@sbermarket.ru>
2022-02-15 21:01:57 +0300

    little dict generate

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import requests
 import json
 
-
-pokemonNamesDict=[]
+counter=1
+pokemonNamesList=[]
+pokemonDict={}
 apiAdr="https://pokeapi.co/api/v2"
 pokemonAdr="pokemon"
-pokeLim=1
+pokeLim=25
 r = requests.get(apiAdr+"/"+pokemonAdr+"?limit="+str(pokeLim))
 jr = r.json()
 
 for i in jr['results']:
-    pokemonNamesDict.append(i['name'])
+    pokemonNamesList.append(i['name'])
 
+#print(pokemonNamesDict)
 
-for i in pokemonNamesDict:
+for i in pokemonNamesList:
     r = requests.get(apiAdr+"/"+pokemonAdr+"/"+i)
+    #jr = r.json().keys()
     jr = r.json()
-    print(jr)
+    pokemonDict[i] = [jr.get('id'), jr.get('height'), jr.get('is_default')]
+
+print('Who is the highest?')
+for name in pokemonNamesList:
+    print(str(counter)+')'+ ' ' +name)
+    counter += 1
+
+#input()
+
+# print(pokemonDict.items())
+    
