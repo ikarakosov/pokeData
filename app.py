076c3fa9f4dfde8e0f28cd61252828416376854e
yuriy.karakosov <yuriy.karakosov@sbermarket.ru>
2022-02-19 12:24:04 +0300

    some logic to work with DB and router

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask
+from crypt import methods
+from turtle import title
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=True)
+
+def __repl__(self):
+    return  '<Article %r>'  % self.id
+
+
 @app.route('/')
 def index():
-    return "this is main page"
+    return render_template("index.html")
 
-
-@app.route('/about')
+@app.route('/create', methods=['POST', 'GET'])
 def about():
-    return "Some info about your mom"
+    if request.method == 'POST':
+        title = request.form['title']
+        article = Article(title=title)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error adding'
+
+    else:
+        return render_template("create.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
